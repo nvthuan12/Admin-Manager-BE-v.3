e07c05ee11d80317c90f18befafb51a5dad85b05
THUANNV <thuannv_tts@rikkeisoft.com>
2023-11-20 13:56:06 +0700

    user controller error fomart

--- flask-api/project/api/common/utils/exceptions.py
+++
@@ -1,8 +0,0 @@
-
-SERVER_ERROR_500 = ({"message": "An error occured."}, 500)
-NOT_FOUND_404 = ({"message": "Resource could not be found."}, 404)
-NO_INPUT_400 = ({"message": "No input data provided."}, 400)
-INVALID_INPUT_422 = ({"message": "Invalid input."}, 422)
-ALREADY_EXIST_409 = ({"message": "Already exists."}, 409)
-UNAUTHORIZED_401 = ({"message": "Wrong credentials."}, 401)
-ACCESS_DENIED_403 = ({"message": "Access denied."}, 403)
