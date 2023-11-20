dbf86fe31fb61e77bc39cc52917e56f652c535eb
ChuongNT <chuongnt_tts@rikkeisoft.com>
2023-11-20 07:51:48 +0700

    Feature Manager Booking

--- flask-api/project/__init__.py
+++ flask-api/project/__init__.py
@@ -23,4 +23,6 @@ with app.app_context():
     from project import models
     db.create_all()
 from project.api.v1.login import login_blueprint
+from project.api.v1.booking_controller import booking_blueprint
 app.register_blueprint(login_blueprint, url_prefix='/v1')
+app.register_blueprint(booking_blueprint, url_prefix='/v1')
