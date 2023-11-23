cc6d09128f26ababfa9ea215eada690e7731f301
THUANNV <thuannv_tts@rikkeisoft.com>
2023-11-23 14:30:14 +0700

    fix api user

--- flask-api/project/api/v1/user_controller.py
+++ flask-api/project/api/v1/user_controller.py
@@ -13,6 +13,73 @@ from project.api.v1.has_permission import has_permission
 
 user_blueprint = Blueprint('user', __name__)
 
+@user_blueprint.route('/users', methods=['GET'])
+@jwt_required()
+@has_permission('view')
+def view_list_user():
+    page = int(request.args.get('page',1)) 
+    per_page = int(request.args.get('per_page',10)) 
+
+    users = User.query.join(UserHasRole).join(Role).join(RoleHasPermission).join(Permission).with_entities(
+        User.user_id,
+        User.user_name,
+        User.email,
+        User.phone_number,
+        UserHasRole.role_id,
+        Role.role_name,
+        RoleHasPermission.permission_id,
+        Permission.permission_name
+    ).all()
+
+    grouped_users = defaultdict(lambda: {
+        "user_id": None,
+        "user_name": None,
+        "email": None,
+        "phone_number": None,
+        "role_id": set(),
+        "role_name": set(),
+        "permission_id": set(),
+        "permission_name": set()
+    })
+
+    for user in users:
+        user_dict = user._asdict()
+        user_id = user_dict["user_id"]
+
+        grouped_users[user_id]["user_id"] = user_dict["user_id"]
+        grouped_users[user_id]["user_name"] = user_dict["user_name"]
+        grouped_users[user_id]["email"] = user_dict["email"]
+        grouped_users[user_id]["phone_number"] = user_dict["phone_number"]
+        grouped_users[user_id]["role_id"].add(user_dict["role_id"])
+        grouped_users[user_id]["role_name"].add(user_dict["role_name"])
+        grouped_users[user_id]["permission_id"].add(user_dict["permission_id"])
+        grouped_users[user_id]["permission_name"].add(user_dict["permission_name"])
+    
+        per_page = per_page
+        start = (page - 1) * per_page
+        end = start + per_page
+        paginated_users = list(grouped_users.values())[start:end]
+        total_pages = len(paginated_users) // per_page + 1
+        total_items = len(paginated_users)
+        
+    result = {
+        'total_pages':total_pages ,
+        'total_items': total_items,
+        'current_page': page,
+        "list_users": [
+        {
+            "user_id": user["user_id"],
+            "user_name": user["user_name"],
+            "email": user["email"],
+            "phone_number": user["phone_number"],
+            "role_id": list(user["role_id"]),
+            "role_name": list(user["role_name"]),
+            "permission_id": list(user["permission_id"]),
+            "permission_name": list(user["permission_name"])
+        }
+        for user in paginated_users
+    ]}
+    return jsonify(result)
 
 @user_blueprint.route('/users', methods=['POST'])
 @jwt_required()
@@ -52,76 +119,9 @@ def create_user():
         db.session.rollback()
         raise  InternalServerError("Failed to create user")
 
-@user_blueprint.route('/users', methods=['GET'])
-@jwt_required()
-@has_permission("view")
-def view_list_user():
-    page = int(request.args.get('page',1)) 
-    per_page = int(request.args.get('per_page',10)) 
-
-    users = User.query.join(UserHasRole).join(Role).join(RoleHasPermission).join(Permission).with_entities(
-        User.user_id,
-        User.user_name,
-        User.email,
-        User.phone_number,
-        UserHasRole.role_id,
-        Role.role_name,
-        RoleHasPermission.permission_id,
-        Permission.permission_name
-    ).all()
-
-    grouped_users = defaultdict(lambda: {
-        "user_id": None,
-        "user_name": None,
-        "email": None,
-        "phone_number": None,
-        "role_id": set(),
-        "role_name": set(),
-        "permission_id": set(),
-        "permission_name": set()
-    })
-
-    for user in users:
-        user_dict = user._asdict()
-        user_id = user_dict["user_id"]
-
-        grouped_users[user_id]["user_id"] = user_dict["user_id"]
-        grouped_users[user_id]["user_name"] = user_dict["user_name"]
-        grouped_users[user_id]["email"] = user_dict["email"]
-        grouped_users[user_id]["phone_number"] = user_dict["phone_number"]
-
-        grouped_users[user_id]["role_id"].add(user_dict["role_id"])
-        grouped_users[user_id]["role_name"].add(user_dict["role_name"])
-        grouped_users[user_id]["permission_id"].add(user_dict["permission_id"])
-        grouped_users[user_id]["permission_name"].add(user_dict["permission_name"])
-    
-        per_page = per_page
-        start = (page - 1) * per_page
-        end = start + per_page
-        paginated_users = list(grouped_users.values())[start:end]
-    
-    result = {
-        'total_pages': len(paginated_users) // per_page + 1,
-        'total_items': len(paginated_users),
-        'current_page': page,
-        "list_users": [
-        {
-            "user_id": user["user_id"],
-            "user_name": user["user_name"],
-            "email": user["email"],
-            "phone_number": user["phone_number"],
-            "role_id": list(user["role_id"]),
-            "role_name": list(user["role_name"]),
-            "permission_id": list(user["permission_id"]),
-            "permission_name": list(user["permission_name"])
-        }
-        for user in paginated_users
-    ]}
-    return jsonify(result)
-
 @user_blueprint.route('/users/<int:user_id>', methods=['PUT'])
 @jwt_required()
-@has_permission("update")
+@has_permission('update')
 def update_user(user_id):
     data = request.get_json()
     user_name = data.get('user_name')
@@ -158,20 +158,23 @@ def update_user(user_id):
 
 @user_blueprint.route('/users/<int:user_id>', methods=['DELETE'])
 @jwt_required()
-@has_permission("delete")
+@has_permission('delete')
 def delete_user(user_id):
     user=User.query.get(user_id)
     if not user:
         raise NotFound("User not found with provided user_id.")
+    try:
+        booking_users = BookingUser.query.filter_by(user_id=user_id).all()
+        for booking_user in booking_users:
+            db.session.delete(booking_user)
 
-    booking_users = BookingUser.query.filter_by(user_id=user_id).all()
-    for booking_user in booking_users:
-        db.session.delete(booking_user)
+        user_has_roles=UserHasRole.query.filter_by(user_id=user_id).all()
+        for user_has_role in user_has_roles:
+            db.session.delete(user_has_role)
 
-    user_has_roles=UserHasRole.query.filter_by(user_id=user_id).all()
-    for user_has_role in user_has_roles:
-        db.session.delete(user_has_role)
-
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify({'message': 'Delete user successfully'}), 200
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'message': 'Delete user successfully'}), 200
+    except Exception as e:
+        db.session.rollback()
+        raise InternalServerError() from e
