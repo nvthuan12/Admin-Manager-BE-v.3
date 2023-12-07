a01c945323d377bd35a8d942a45dd2e438dd26ad
ChuongNT99 <chuong_nt@rikkeisoft.com>
2023-12-07 12:38:43 +0700

    accept/reject invition belong to user

--- flask-api/project/api/v1/booking_controller.py
+++ flask-api/project/api/v1/booking_controller.py
@@ -253,3 +253,43 @@ def view_list_invite() -> dict:
         return BaseResponse.error(e)
     except InternalServerError as e:
         return BaseResponse.error(e)
+    
+@booking_blueprint.route("/user/bookings/<int:booking_id>/accept", methods=["PUT"])
+@jwt_required()
+@has_permission("update")
+def user_accept_booking_endpoint(booking_id: int):
+    try:
+        response_data = BookingService.user_accept_booking(booking_id)
+        return response_data
+
+    except BadRequest as e:
+        return BaseResponse.error(e)
+
+    except Conflict as e:
+        return BaseResponse.error(e)
+
+    except NotFound as e:
+        return BaseResponse.error(e)
+
+    except InternalServerError as e:
+        return BaseResponse.error(e)
+
+@booking_blueprint.route("/user/bookings/<int:booking_id>/reject", methods=["PUT"])
+@jwt_required()
+@has_permission("update")
+def user_reject_booking_endpoint(booking_id: int):
+    try:
+        response_data = BookingService.user_reject_booking(booking_id)
+        return response_data
+
+    except BadRequest as e:
+        return BaseResponse.error(e)
+
+    except Conflict as e:
+        return BaseResponse.error(e)
+
+    except NotFound as e:
+        return BaseResponse.error(e)
+
+    except InternalServerError as e:
+        return BaseResponse.error(e)
