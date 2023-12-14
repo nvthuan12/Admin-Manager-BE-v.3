cffa398a484a267560a6a3c297acb299a9f517a3
ChuongNT99 <chuong_tts@rikkeisoft.com>
2023-12-14 17:01:22 +0700

    notify admin accept booking to user

--- flask-api/project/services/email_service.py
+++ flask-api/project/services/email_service.py
@@ -9,7 +9,7 @@ from project.models import User, Booking
 
 class EmailSender:
     @celery.task
-    async def send_email_inviting_join_the_meeting(user_email, title, time_start, time_end, room_name, attendees):
+    def send_email_inviting_join_the_meeting(user_email, title, time_start, time_end, room_name, attendees):
         try:
                 msg = Message(f'[LỜI MỜI THAM GIA]: {title}', sender=os.getenv('MAIL_USERNAME'), recipients=[user_email])
                 msg.body = f'Thông báo cuộc họp\n\n' \
@@ -17,23 +17,23 @@ class EmailSender:
                            f'Thời gian: {time_start} - {time_end}\n' \
                            f'Người tham gia:\n{",\n".join(attendees)}\n\n' \
                            f'Bạn được thêm vào tham gia cuộc họp. Vào trang lời mời tham gia cuộc họp của mình để xác nhận tham gia.'
-                await mail.send.apply_async(args=[msg])
+                mail.send(msg)
                 
         except smtplib.SMTPException as e:
-            return await BaseResponse.error(e)
+            return BaseResponse.error(e)
         
     @celery.task
-    async def send_email_accepting_the_scheduled(user_email, title, time_start, time_end, room_name, attendees):
+    def send_email_accepting_the_scheduled(user_email, title, time_start, time_end, room_name, attendees):
         try:
                 msg = Message(f'[XÁC NHẬN]: {title}', sender=os.getenv('MAIL_USERNAME'), recipients=[user_email])
                 msg.body = f'Có một cuộc họp đã được chấp nhận!\n\n' \
                            f'Phòng họp: {room_name}\n' \
                            f'Thời gian: {time_start} - {time_end}\n' \
                            f'Người tham gia:\n{",\n".join(attendees)}' 
-                await mail.send.apply_async(args=[msg])
+                mail.send(msg)
 
         except smtplib.SMTPException as e:
-            return await BaseResponse.error(e)
+            return BaseResponse.error(e)
     
     @staticmethod
     def send_mail_reminder(booking: Booking, user: User):
@@ -50,5 +50,5 @@ class EmailSender:
                            f'Người tham gia:\n{",\n".join(attendees)}' 
                 mail.send(msg)
                 
-        except InternalServerError as e:
+        except smtplib.SMTPException as e:
             return BaseResponse.error(e)
